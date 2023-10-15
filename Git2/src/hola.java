e85afeb2b3bca94bcc64877b9fc2a7d372a697d1
Miquel <mpetrus@iesjoanramis.org>
2023-10-15 22:43:11 +0200

    Third commit

--- Git2/src/hola.java
+++ Git2/src/hola.java
@@ -4,6 +4,7 @@ public class hola {
 	public static void main(String[] args) {
 		System.out.println("Hola git");
 		System.out.println("Hola git again");
+		System.out.println("Hola git again 2");
 
 	}
 
